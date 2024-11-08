9a147fb01a2c59061cf6c70da75f5defc9269180
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-11-08 09:44:56 +0100

    add session flash messages to take care of success messages

--- app/controllers/auth/register_controller.ts
+++ app/controllers/auth/register_controller.ts
@@ -6,12 +6,17 @@ export default class RegisterController {
   async show({ view }: HttpContext){
     return view.render('pages/auth/register')
   }
-  async store({ auth, request, response}: HttpContext){
+  async store({ auth, request, response, session }: HttpContext){
     const data = await request.validateUsing(registerValidator)
 
     const staff = await Staff.create(data)
 
     await auth.use('web').login(staff)
+
+    session.flash('notification', {
+      type: 'success',
+      message: 'Registration Successful',
+    })
     
     return response.redirect('/')
   }
