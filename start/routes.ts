ce9f9923b79e6c727b5edbb5e3fe4e17c9ab573a
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-11-08 07:48:27 +0100

    update the routes for staff registration

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,7 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+const RegisterController = () => import('#controllers/auth/register_controller')
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('auth/register', [RegisterController, 'show'])
+router.post('auth/register', [RegisterController, 'store'])
